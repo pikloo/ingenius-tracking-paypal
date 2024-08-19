2d2dc35d4200e1ffd88a4513aa6bafdac1c978b9
pikloo <p.loukakou@gmail.com>
2024-08-19 02:16:29 +0200

    'envoie du tracking vérifié'

--- admin/class-ingenius-tracking-paypal-aftership-order.php
+++ admin/class-ingenius-tracking-paypal-aftership-order.php
@@ -3,10 +3,6 @@
 namespace IngeniusTrackingPaypal\Admin;
 
 use WooCommerce\PayPalCommerce\ApiClient\Exception\RuntimeException;
-use WooCommerce\PayPalCommerce\Button\Endpoint\RequestData;
-use WooCommerce\PayPalCommerce\OrderTracking\Endpoint\OrderTrackingEndpoint;
-use WooCommerce\PayPalCommerce\OrderTracking\Shipment\ShipmentFactory;
-use WooCommerce\WooCommerce\Logging\Logger\NullLogger;
 use WP_Error;
 
 defined('ABSPATH') || exit;
@@ -88,100 +84,41 @@ if (!class_exists('Ingenius_Tracking_Paypal_Aftership_Order')) {
 
             $order = wc_get_order($this->order_id);
 
-            //TODO: Récupérer les API credentials créé dans Paypal
-            // Récupérer les identifiants Paypal
-            $client_id = 'AU7ebfDwPJ2uP_yShWzwQ1fMrTPZ_OvJ9ivd4OJZx2JyERardyWCLquQ2d6fc7uj1q3YIWhmoY4N9hud';
-            $client_secret = 'EGjyyRS3AK06h9gUnuH2z9Gt1-PJ7CSEc3Akfar75cXCb7uUa5gSJl-Ye6RPNXgZzAe4lkzpLWTFn7y1';
+            // Récupérer les credentials API Paypal
+            $paypal_settings = get_option('woocommerce-ppcp-settings');
+            $client_id = isset($paypal_settings['sandbox_on']) && $paypal_settings['sandbox_on'] ? $paypal_settings['client_id_sandbox'] : $paypal_settings['client_id_production'];
+            $client_secret = isset($paypal_settings['sandbox_on']) && $paypal_settings['sandbox_on'] ? $paypal_settings['client_secret_sandbox'] : $paypal_settings['client_secret_production'];
 
             require_once plugin_dir_path(__FILE__) . 'class-ingenius-tracking-paypal-api-connection.php';
             // Instanciation de la connexion PayPal
             $paypal_connection = new PayPalConnection($client_id, $client_secret);
 
-            //TODO: Si le numéro de tracking et le carrier est changé faire l'update
-            error_log('avant connexion');
             try {
                 // Obtenir le Bearer Token
                 $paypal_token = $paypal_connection->it_get_paypal_bearer_token();
                 $paypal_token_data = json_decode($paypal_token);
 
-                //Créer un objet json pour le token
-                $token_json = (object)[
-                    'token' => $paypal_token_data->access_token, // Remplacez par le token réel
-                    'token_type' => $paypal_token_data->token_type,
-                    'expires_in' => $paypal_token_data->expires_in,  // Durée de vie du token en secondes
-                    'scope' => $paypal_token_data->scope,
-                    'created' => time()  // Temps de création du token
-                ];
-
-                require_once plugin_dir_path(__FILE__) . 'class-ingenius-tracking-paypal-bearer.php';
-
-                $bearer = new BearerToken($token_json);
-
-                $shipment_factory = new ShipmentFactory(); // Exemple, cela peut nécessiter plus de configuration
-                $allowed_statuses = ['SHIPPED', 'ON_HOLD', 'DELIVERED', 'CANCELLED'];
-                $should_use_new_api = true;
-
-                // $host = $paypal_connection->it_is_paypal_sandbox_mode() 
-                // ? 'https://api.sandbox.paypal.com' 
-                // : 'https://api.paypal.com';
-
-                $host =  'https://api.sandbox.paypal.com'; //TEST
-
-
-                $order_tracking_endpoint = new OrderTrackingEndpoint(
-                    $host,
-                    $bearer,
-                    new NullLogger(), // Logger de base, vous pouvez utiliser un logger personnalisé,
-                    new RequestData(), // Exemple, cela peut nécessiter plus de configuration,
-                    $shipment_factory,
-                    $allowed_statuses,
-                    $should_use_new_api
-                );
-
-
-                // Préparer les données à envoyer à l'API PayPal
                 $tracking_data = [
                     'tracking_number' => $this->tracking_number,
                     'carrier'         => isset(self::CARRIERS_NAME[$this->carrier_name]) ? self::CARRIERS_NAME[$this->carrier_name] : 'OTHER',
-                    'carrier_name_other' => isset(self::CARRIERS_NAME[$this->carrier_name]) ? '' : $this->carrier_name,
                     'capture_id' => $order->get_transaction_id(),
                     'items' => $this->items,
                 ];
 
+                if ($tracking_data['carrier'] === 'OTHER') {
+                    $tracking_data['carrier_name_other'] = $this->carrier_name;
+                }
+
                 if (isset(self::ORDER_STATUS[$order->get_status()])) {
                     $tracking_data['status'] = self::ORDER_STATUS[$order->get_status()];
                 }
-
-                // Création d'une instance de Shipment
-                $shipment = $shipment_factory->create_shipment(
-                    $this->order_id,
-                    $tracking_data['capture_id'],
-                    $tracking_data['tracking_number'],
-                    isset($tracking_data['status']) ?? '',
-                    $tracking_data['carrier'],
-                    $tracking_data['carrier_name_other'],
-                    $tracking_data['items']
-                );
-
-                error_log("Commande #{$this->order_id} capture_id #{$tracking_data['capture_id']}  status #{$tracking_data['status']} carrier #{$tracking_data['carrier']} other #{$tracking_data['carrier_name_other']}");
-
-                // Appel à add_tracking_information ou update_tracking_information
-                try {
-                    // Si tracking data chez Paypal update sinon add
-                    if ($order->get_meta('_ppcp_paypal_order_id')) {
-                        $order_tracking_endpoint->update_tracking_information($shipment, $this->order_id);
-                    } else {
-                        $order_tracking_endpoint->add_tracking_information($shipment, $this->order_id);
-                    }
-                    error_log('Tracking information updated successfully!');
-                    echo 'Tracking information added successfully!';
-                } catch (RuntimeException $e) {
-                    error_log($e->getMessage());
-                    echo 'Error adding tracking information: ' . $e->getMessage();
+                //Voir si un tracking comportant le num de transaction et le tracking number existe
+                $order_tracking_response = $paypal_connection->it_get_order_tracking($order->get_transaction_id(), $this->tracking_number, $paypal_token_data->access_token);
+                if ($order_tracking_response['code'] >= 400) {
+                    $paypal_connection->it_add_order_tracking($order->get_meta('_ppcp_paypal_order_id'), $tracking_data, $paypal_token_data->access_token);
+                } else if ($order_tracking_response === 200) {
+                    $paypal_connection->it_update_order_tracking($order->get_transaction_id(), $tracking_data, $paypal_token_data->access_token);
                 }
-
-                // Utiliser le Bearer Token comme requis par votre application
-                // Par exemple, en passant le token à une autre classe ou en le stockant pour une utilisation ultérieure
             } catch (RuntimeException $e) {
                 // Gestion des erreurs
                 error_log($e->getMessage());
--- admin/class-ingenius-tracking-paypal-api-connection.php
+++ admin/class-ingenius-tracking-paypal-api-connection.php
@@ -21,10 +21,10 @@ class PayPalConnection
     {
         $this->client_id = $client_id;
         $this->client_secret = $client_secret;
-        $this->api_url = 'https://api.sandbox.paypal.com'; //TEST
-        // $this->api_url = $this->it_is_paypal_sandbox_mode() 
-        // ? 'https://api.sandbox.paypal.com' 
-        // : 'https://api.paypal.com';
+        // $this->api_url = 'https://api.sandbox.paypal.com'; //TEST
+        $this->api_url = $this->it_is_paypal_sandbox_mode()
+            ? 'https://api.sandbox.paypal.com'
+            : 'https://api.paypal.com';
     }
 
     /**
@@ -67,38 +67,64 @@ class PayPalConnection
     }
 
 
-    public function it_is_paypal_sandbox_mode() {
+    public static function it_is_paypal_sandbox_mode()
+    {
         // Récupère les paramètres de PayPal dans WooCommerce
-        $paypal_settings = get_option('woocommerce_paypal_settings');
-    
+        $paypal_settings = get_option('woocommerce-ppcp-settings');
+
         // Vérifie si le mode sandbox est activé
-        if (isset($paypal_settings['testmode']) && $paypal_settings['testmode'] === 'yes') {
+        if (isset($paypal_settings['sandbox_on']) && $paypal_settings['sandbox_on']) {
             return true; // Mode sandbox activé
         }
-    
+
         return false; // Mode live activé
     }
 
-    // public function it_get_paypal_order_informations($access_token, $order_id)
-    // {
-    //     $ch = curl_init();
-    //     curl_setopt_array($ch, [
-    //         CURLOPT_URL => $this->api_url . '/v2/checkout/orders/' . $order_id,
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_HTTPHEADER => [
-    //             "Authorization: Bearer {$access_token}",
-    //             "Content-Type: application/json",
-    //         ],
-    //     ]);
+    public function it_get_order_tracking($transaction_id, $tracking_number, $access_token)
+    {
+        return $this->it_handle_paypal_request('/v1/shipping/trackers/', $access_token, "{$transaction_id}-{$tracking_number}");
+    }
 
-    //     $response = curl_exec($ch);
-    //     curl_close($ch);
+    public function it_add_order_tracking($paypal_order_id, $tracking_data,  $access_token)
+    {
+        return $this->it_handle_paypal_request("/v2/checkout/orders/{$paypal_order_id}/track",  $access_token, null, $tracking_data);
+    }
 
-    //     if ($response === false) {
-    //         throw new RuntimeException('Erreur lors de la récupération des détails de la commande PayPal');
-    //     }
+    public function it_update_order_tracking($transaction_id, $tracking_data,  $access_token){
+        return $this->it_handle_paypal_request("/v1/shipping/trackers/", $access_token, "{$transaction_id}-{$tracking_data["tracking_number"]}", $tracking_data);
+    }
 
-    //     // Décoder la réponse pour obtenir les détails de la commande
-    //     return $response;
-    // }
+    protected function it_handle_paypal_request($endpoint, $access_token, $params = null, $body = null)
+    {
+        $ch = curl_init();
+        $url = $this->api_url . $endpoint;
+        if ($params) {
+            $url .= '/' . $params;
+        }
+        $options = [
+            CURLOPT_URL => $this->api_url . $endpoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => [
+                "Authorization: Bearer {$access_token}",
+                "Content-Type: application/json",
+            ]
+        ];
+
+        if ($body) {
+            $options[CURLOPT_POSTFIELDS] = json_encode($body);
+        }
+
+        curl_setopt_array($ch, $options);
+
+        $response = curl_exec($ch);
+        $http_status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+    
+        // Décoder la réponse pour obtenir les détails de la commande
+        return [
+            'code' => $http_status_code,
+            'response'=> $response    
+        ];
+    }
 }
